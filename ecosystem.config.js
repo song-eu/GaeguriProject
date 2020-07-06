1b2790a915de86d0040905d9dee84c71e5e6ec94
MAC <mac@ara.local>
2020-07-06 22:03:40 +0700

    for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,6 @@ module.exports = {
 			script: 'npm', // 실행할 파일 경로
 			args: 'start',
 			instances: 1, // 클러스터 모드 사용 시 생성할 인스턴스 수
-			exec_mode: 'cluster', // fork, cluster 모드 중 선택
-			merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
 			autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
 			watch: false, // 파일이 변경되었을 때 재시작 할지 선택
 			max_memory_restart: '512M', // 프로그램의 메모리 크기가 일정 크기 이상이 되면 재시작한다.
@@ -27,7 +25,7 @@ module.exports = {
 		development: {
 			// 개발 환경설정
 			user: 'root',
-			host: [{ host: 'http://52.78.76.186/', port: '4000' }],
+			host: [{ host: 'http://52.78.76.186', port: '4000' }],
 			ssh_options: 'StrictHostKeyChecking=no',
 			ref: 'origin/Dev', // 리모트 브랜치
 			repo: 'git@github.com:codestates/Gaeguri-server.git', // Github 프로젝트 주소
